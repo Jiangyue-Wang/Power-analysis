6fc104673339c60d74be72171052355cbf01bc90
Jiangyue-Wang <lily_wang@pku.edu.cn>
2023-12-04 22:36:12 +0800

    finalize results

--- README.md
+++ README.md
@@ -1,41 +1,34 @@
----
-editor_options: 
-  markdown: 
-    wrap: 72
----
+# Power-analysis
 
-Power-analysis
+Data is obtained from the supplementary material of [Kormann et al.]( https://doi.org/10.1002/eap.2441) (2021), can be found [here](data/overview_herbicide_biodiv_effects.csv). The values do not stand for actual values of species richness, they are the estimated coeficients from linear mixed model outputs. SDs are calculated by dividing the difference between coef estimates and lower/upper 95% CI limits by 1.96.
 
-# Question1: Post-hoc power calculation
+Detailed code used for analysis is [here](src/03-0.05_intercept.R).
+
+## Question1: Post-hoc power calculation
 
 Detailed results see [here](results/03-power.csv).
 
-# Question2: Minimum sample size required for 80% power
+## Question2: Minimum sample size required for 80% power
 
 -   Woody, Light \~ Control
 
 ![Woody, Light Group versus Control](results/03_woody_CL.png)
 
-
 -   Woody, Moderate \~ Control
 
 ![Woody, Moderate Group versus Control](results/03_woody_CM.png)
-
 
 -   Woody, Intensive \~ Control
 
 ![Woody, Intensive Group versus Control](results/03_woody_CI.png)
 
-
 -   Flower, Light \~ Control
 
 ![Flower, Light Group versus Control](results/03_flowr_CL.png)
 
-
 -   Flower, Moderate \~ Control
 
 ![Flower, Moderate Group versus Control](results/03_flowr_CM.png)
-
 
 -   Flower, Intensive \~ Control
 
--- src/03-0.05_intercept.R
+++ src/03-0.05_intercept.R
@@ -5,21 +5,24 @@ library(fixest)
 library(broom)
 
 ### set params ----
-pset <- 0.05
+pset <- 0.05 # alpha level set as 0.05
 ### read in data----
-#Here we used real richness value, and assume a t test comparison without correction for p
+#Here we used mixed effect model estimates coefficient, and assume a simple OLS without correction for p
 data <- read_csv("data/overview_herbicide_biodiv_effects.csv")
 data %<>% mutate(Treatment = factor(Treatment, levels = c("CONTROL", "LIGHT", "MODERATE", "INTENSIVE"))) %>% filter(taxon %in% c("woodies", "flowers")) %>% select(pred_sr, lower_sr) %>% mutate(sd_sr = ((pred_sr-lower_sr)/1.96))
 
 Distri_params <- data.frame(Treatment = factor(c("CONTROL", "LIGHT", "MODERATE", "INTENSIVE"), levels = c("CONTROL", "LIGHT", "MODERATE", "INTENSIVE")), woody = data[1:4, "pred_sr"], woody_sd = data[1:4, "sd_sr"], flowr = data[5:8, "pred_sr"], flowr_sd = data[5:8,"sd_sr"])
+
 colnames(Distri_params)[2:5] <- c("woody","woody_sd","flowr","flowr_sd")
-sample_size <- 7
+
+sample_size <- 7 # seven blocks
 
 
 ### calculate power----
 post_power <- data.frame(Group = c("CL", "CM", "CI"), woody_power = NA, flowr_power = NA)
 
-# function to test power, iteration 999 times
+# function to test power, iteration 999 times----
+# copy code from manual
 power_test <- function(effect, sd, sample_size){
   sig_results <- c()
   
@@ -42,32 +45,42 @@ power_test <- function(effect, sd, sample_size){
     return()
 }
 
+# calculate power for three groups, two taxon----
 for(i in 1:3){
+  # get effect size for woody in light, moderate, intensive groups
   w_eff <- Distri_params$woody[i+1]-Distri_params$woody[1]
+  # get sd for woody in light, moderate, intensive groups
   w_sd <- max(Distri_params$woody_sd[1])
+  # calculate power
   post_power$woody_power[i] <- power_test(effect = w_eff, sd = w_sd, sample_size = sample_size)
-  
+  # get effect size for flower in light, moderate, intensive groups
   f_eff <- Distri_params$flowr[i+1]-Distri_params$flowr[1]
+  # get sd for flower in light, moderate, intensive groups
   f_sd <- max(Distri_params$flowr_sd[1])
+  # calculate power
   post_power$flowr_power[i] <- power_test(effect = f_eff, sd = f_sd, sample_size = sample_size)
 }
-
+ # store results
 write.csv(post_power,"results/03-power.csv",row.names = F)
+
 # calculate needed sample size----
 sample_sizes_to_try <- c(2,3,4,5,7,10,15,20,30,40,50,100,200,300,400,500)
 
+
 for(i in 1:3){
+  # for three groups, two taxon, get effect sizes and sd
   w_eff <- Distri_params$woody[i+1]-Distri_params$woody[1]
   w_sd <- max(Distri_params$woody_sd[1])
   f_eff <- Distri_params$flowr[i+1]-Distri_params$flowr[1]
   f_sd <- max(Distri_params$flowr_sd[1])
-  # woody first
+  # woody first, calculate power along sample size gradient
   power_levels <- c()
   for(j in 1: length(sample_sizes_to_try)){
     power_levels[j] <- power_test(effect = w_eff, sd = w_sd, sample_size = sample_sizes_to_try[j])
   }
   power_results <- tibble(sample = sample_sizes_to_try,
                           power = power_levels)
+  # plot curves
   ggplot(power_results, 
          aes(x = sample, y = power)) +
     geom_line(color = 'red', size = 1.5) + 
@@ -77,15 +90,16 @@ for(i in 1:3){
     theme_bw() + 
     scale_y_continuous(labels = scales::percent) + 
     labs(x = 'Sample Size', y = 'Power')
-  ggsave(paste0("results/03_woody_",post_power$Group[i],".png"), width = 6, height = 4)
-  saveRDS(power_results,paste0("results/03_woody_",post_power$Group[i],".rds"))
-  # flower second
+  ggsave(paste0("results/03_woody_",post_power$Group[i],".png"), width = 6, height = 4) # save plot
+  saveRDS(power_results,paste0("results/03_woody_",post_power$Group[i],".rds")) # save results
+  # flower second, calculate power along sample size gradient
   power_levels <- c()
   for(j in 1: length(sample_sizes_to_try)){
     power_levels[j] <- power_test(effect = f_eff, sd = f_sd, sample_size = sample_sizes_to_try[j])
   }
   power_results <- tibble(sample = sample_sizes_to_try,
                           power = power_levels)
+  # plot curve
   ggplot(power_results, 
          aes(x = sample, y = power)) +
     geom_line(color = 'red', size = 1.5) + 
@@ -95,6 +109,6 @@ for(i in 1:3){
     theme_bw() + 
     scale_y_continuous(labels = scales::percent) + 
     labs(x = 'Sample Size', y = 'Power')
-  ggsave(paste0("results/03_flowr_",post_power$Group[i],".png"), width = 6, height = 4)
-  saveRDS(power_results,paste0("results/03_flowr_",post_power$Group[i],".rds"))
+  ggsave(paste0("results/03_flowr_",post_power$Group[i],".png"), width = 6, height = 4) # store plot
+  saveRDS(power_results,paste0("results/03_flowr_",post_power$Group[i],".rds")) # store results
 }
